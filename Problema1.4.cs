b90b1fc3f9599d07efcefbae50c0a00f11e5cf1d
stefanpi <45466337+stefanpi@users.noreply.github.com>
2019-07-18 23:49:14 +0300

    Add files via upload

---
+++ Problema1.4.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+/// <summary>
+/// Summary description for WebService
+/// </summary>
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+ [System.Web.Script.Services.ScriptService]
+public class WebService : System.Web.Services.WebService
+{
+
+    public WebService()
+    {
+
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public bool  ValidationXML(string fileName)
+    {
+        string fileNameWithoutExtention = fileName.Split('.')[0];
+
+        if (fileNameWithoutExtention == "valid")
+        {
+            Context.Response.Status = "200 ok";
+            Context.Response.StatusCode = 200;
+            Context.ApplicationInstance.CompleteRequest();
+            return true;
+        }
+        else
+        {
+            Context.Response.Status = "500 !internal serval error";
+            Context.Response.StatusCode = 500;
+            Context.ApplicationInstance.CompleteRequest();
+            return false;
+        }
+    }
+}
